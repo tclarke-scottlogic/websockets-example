7af652509908c1d5ba73e5c903e38a1e7593b006
Thomas Clarke <TClarke@scottlogic.com>
2018-01-04 10:58:19 +0000

    moved to client folder

--- client/app.js
+++ client/app.js
@@ -20,10 +20,10 @@ window.onload = function() {
 
   // Show a connected message when the WebSocket is opened.
   socket.onopen = function(event) {
-    socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
+    console.info(event);
+    socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url;
     socketStatus.className = 'open';
   };
-
 
   // Handle messages sent by the server.
   socket.onmessage = function(event) {
@@ -32,13 +32,11 @@ window.onload = function() {
                                message + '</li>';
   };
 
-
   // Show a disconnected message when the WebSocket is closed.
   socket.onclose = function(event) {
     socketStatus.innerHTML = 'Disconnected from WebSocket.';
     socketStatus.className = 'closed';
   };
-
 
   // Send a message when the form is submitted.
   form.onsubmit = function(e) {
@@ -46,6 +44,7 @@ window.onload = function() {
 
     // Retrieve the message from the textarea.
     var message = messageField.value;
+    console.info(message);
 
     // Send the message through the WebSocket.
     socket.send(message);
@@ -60,7 +59,6 @@ window.onload = function() {
     return false;
   };
 
-
   // Close the WebSocket connection when the close button is clicked.
   closeBtn.onclick = function(e) {
     e.preventDefault();
@@ -70,5 +68,4 @@ window.onload = function() {
 
     return false;
   };
-
 };
